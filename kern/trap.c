1493baf66ae386f01d7268ba07010ac9f3025452
hzqmwne <huangzhengqmwne@sina.cn>
2018-05-26 00:03:22 +0800

    lab6 start

--- kern/trap.c
+++ kern/trap.c
@@ -187,7 +187,7 @@ trap_dispatch(struct Trapframe *tf)
 	// Handle clock interrupts. Don't forget to acknowledge the
 	// interrupt using lapic_eoi() before calling the scheduler!
 	// LAB 4: Your code here.
-
+ 
 	// Add time tick increment to clock interrupts.
 	// Be careful! In multiprocessors, clock interrupts are
 	// triggered on every CPU.
